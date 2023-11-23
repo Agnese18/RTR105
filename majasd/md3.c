87f752ae4c6380f01a6c57527bbd0cfee122eb7c
Agnese18 <78478660+Agnese18@users.noreply.github.com>
2023-11-23 19:24:45 +0200

    Update md3.c

--- majasd/md3.c
+++ majasd/md3.c
@@ -1,41 +1,41 @@
 #include<stdio.h>
 int main(){
-    int a, b, c;
-printf("ievadi skaitli a=");
-scanf("%d\n", &a);
-printf("ievadi skaitli b=");
-scanf("%d\n", &b);
-printf("ievadi skaitli c=");
-scanf("%d\n", &c);
+    int a, b, c, result1, result2, result3;
+printf("ievadi skaitli a=\n");
+scanf("%d", &a);
+printf("ievadi skaitli b=\n");
+scanf("%d", &b);
+printf("ievadi skaitli c=\n");
+scanf("%d", &c);
 
 if (a>b && a>c){
-        int result1 = a;
-        if (b>c){
-            int result2 = b;
-            int result3 = c;
-        } else {
-            int result2 = c;
-            int result3 = b;
+        printf(" %d\n", a);
+if (b>c){
+           printf(" %d\n",b);
+           printf(" %d\n", c);
+} else {
+            printf(" %d\n", c);
+           printf(" %d\n",b);
         }
     }
     if (b>a && b>c){
-        int result1 = b;
+        printf(" %d\n", b);
         if (a>c){
-            int result2 = a;
-            int result3 = c;
+            printf(" %d\n", a);
+            printf(" %d\n", c);
         } else {
-            int result2 = c;
-            int result3 = a;
+            printf(" %d\n", c);
+            printf(" %d\n",a);
         }
     }
     if (c>a && c>b){
-        int result1 = c;
+        printf(" %d\n", c);
         if(a>b){
-            int result2 = a;
-            int result3 = b;
+            printf(" %d\n", a);
+            printf(" %d\n", b);
         } else {
-            int result2 = b;
-            int result3 = a;
+            printf(" %d\n", b);
+            printf(" %d\n", a);
         }
 
 }
