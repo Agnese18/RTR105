2ed451aff70590636696a0a98caeaa9fba532b48
Agnese18 <78478660+Agnese18@users.noreply.github.com>
2023-12-02 17:11:21 +0200

    Add files via upload

---
+++ majasd/md41.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int number,a, i, factorial;
+	factorial = 1;
+
+
+    printf("izvelies datu tipu - ja char ievadi (1), ja int tad(2), ja longlong (3) \n");
+	scanf("%d",&number);
+
+	printf("ievadi nummuru= \n");
+	scanf("%d",&number);
+
+if (a==1){  //char tips
+    printf("nummurs char_datu tipam ir =%ld \n", number);
+
+    while (number > 0)
+    
+    (i = 1; i <= a; ++i) 
+        factorial *= i;
+        
+        printf("faktorials no %ld = %llu \n", a, factorial);
+
+    return 0;
+
+
+}
+
+if (a==2){  //int tips
+    printf("nummurs char_datu tipam ir =%d \n", number);
+    while 
+}
+
+if (a==1){  //long long tips
+    printf("nummurs char_datu tipam ir =%lld \n", number);
+    while 
+}
+}
